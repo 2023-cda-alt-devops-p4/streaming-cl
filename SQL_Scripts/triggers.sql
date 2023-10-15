280213ccc42ee778ad5d32909317a875838b6618
CamilleLafrance <cam.lafr@free.fr>
2023-10-15 22:32:05 +0200

    feat: change & test triggers

--- SQL_Scripts/triggers.sql
+++ SQL_Scripts/triggers.sql
@@ -1,5 +1,4 @@
 -- TO TRACK ALL CHANGES MADE TO THE USERS TABLE
-
 -- Create an archive table
 CREATE TABLE Users_Archive (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -11,101 +10,38 @@ CREATE TABLE Users_Archive (
 );
 
 -- Create a trigger that runs every time a change is made in the users table
-
 -- Update
-DELIMITER / / CREATE TRIGGER Users_Update_Trigger
-AFTER
-UPDATE ON Users FOR EACH ROW BEGIN
-INSERT INTO Users_Archive (
-        user_id,
-        updated_at,
-        action,
-        old_value,
-        new_value
-    )
-VALUES (
-        NEW.id,
-        NOW(),
-        'UPDATE',
-        CONCAT(
-            'first_name: ',
-            OLD.first_name,
-            ', last_name: ',
-            OLD.last_name,
-            ', email: ',
-            OLD.email,
-            ', password: ',
-            OLD.password
-        ),
-        CONCAT(
-            'first_name: ',
-            NEW.first_name,
-            ', last_name: ',
-            NEW.last_name,
-            ', email: ',
-            NEW.email,
-            ', password: ',
-            NEW.password
-        )
-    );
+DELIMITER //
+CREATE TRIGGER Users_Update
+AFTER UPDATE ON Users
+FOR EACH ROW
+BEGIN
+    INSERT INTO Users_Archive (user_id, updated_at, action, old_value, new_value)
+    VALUES (NEW.id, NOW(), 'UPDATE', CONCAT('first_name: ', OLD.first_name, ', last_name: ', OLD.last_name, ', email: ', OLD.email, ', password: ', OLD.password), CONCAT('first_name: ', NEW.first_name, ', last_name: ', NEW.last_name, ', email: ', NEW.email, ', password: ', NEW.password));
 END;
-/ / DELIMITER;
+//
+DELIMITER ;
 
 -- Insert
-DELIMITER / / CREATE TRIGGER Users_Insert_Trigger
-AFTER
-INSERT ON Users FOR EACH ROW BEGIN
-INSERT INTO Users_Archive (
-        user_id,
-        date_modification,
-        action,
-        ancienne_valeur,
-        nouvelle_valeur
-    )
-VALUES (
-        NEW.id,
-        NOW(),
-        'INSERT',
-        NULL,
-        CONCAT(
-            'first_name: ',
-            NEW.first_name,
-            ', last_name: ',
-            NEW.last_name,
-            ', email: ',
-            NEW.email,
-            ', password: ',
-            NEW.password
-        )
-    );
+DELIMITER //
+CREATE TRIGGER Users_Insert
+AFTER INSERT ON Users
+FOR EACH ROW
+BEGIN
+    INSERT INTO Users_Archive (user_id, updated_at, action, old_value, new_value)
+    VALUES (NEW.id, NOW(), 'INSERT', NULL, CONCAT('first_name: ', NEW.first_name, ', last_name: ', NEW.last_name, ', email: ', NEW.email, ', password: ', NEW.password));
 END;
-/ / DELIMITER;
+//
+DELIMITER ;
 
 -- Delete
-DELIMITER / / CREATE TRIGGER Users_Delete_Trigger
-AFTER DELETE ON Users FOR EACH ROW BEGIN
-INSERT INTO Users_Archive (
-        user_id,
-        date_modification,
-        action,
-        ancienne_valeur,
-        nouvelle_valeur
-    )
-VALUES (
-        OLD.id,
-        NOW(),
-        'DELETE',
-        CONCAT(
-            'first_name: ',
-            OLD.first_name,
-            ', last_name: ',
-            OLD.last_name,
-            ', email: ',
-            OLD.email,
-            ', password: ',
-            OLD.password
-        ),
-        NULL
-    );
+DELIMITER //
+CREATE TRIGGER Users_Delete
+AFTER DELETE ON Users
+FOR EACH ROW
+BEGIN
+    INSERT INTO Users_Archive (user_id, updated_at, action, old_value, new_value)
+    VALUES (OLD.id, NOW(), 'DELETE', CONCAT('first_name: ', OLD.first_name, ', last_name: ', OLD.last_name, ', email: ', OLD.email, ', password: ', OLD.password), NULL);
 END;
-/ / DELIMITER;
+//
+DELIMITER ;
